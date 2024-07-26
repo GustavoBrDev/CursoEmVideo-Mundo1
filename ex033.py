38d901e233145556337f680af2f33040c08f89dd
Gustavo Stinghen <116108689+GustavoBrDev@users.noreply.github.com>
2024-07-26 15:10:16 -0300

    Adição - Exercício 33
    Adição do código que implementa o exercício do 33 da lista de exercícios 
    
    Exercício Python 33: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.

---
+++ ex033.py
@@ -0,0 +1,18 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+
+# Verificiando quem é o menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+# Verficando quem é o maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print(f'O \033[1;35mmaior\033[m numero é {maior}')
+print(f'O \033[1;34mmenor\033[m número é {menor}')

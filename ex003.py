cb36661014a3e60e01a687f2184b6d6c9443e54e
Gustavo Stinghen <116108689+GustavoBrDev@users.noreply.github.com>
2024-07-26 13:57:28 -0300

    Adição - Exercício 3
    Adição do código que implementa o exercício do 3 da lista de exercícios 
    
    Exercício  Python 3: Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ ex003.py
@@ -0,0 +1,5 @@
+num1 = float ( input ( "Informe o primeiro número: ") )
+num2 = float  ( input ( "Informe o segundo número: ") )
+soma = num1 + num2;
+
+print( f"A soma dos números {num1:.2f} e {num2:.2f} é {soma:.2f}" )
